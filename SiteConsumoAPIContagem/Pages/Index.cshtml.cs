e0a2190991b5cda13d9c4329a4aa95b40d485203
Renato Groffe <renato.groffe@yahoo.com.br>
2020-08-01 12:20:08 -0300

    ASP.NET Core 3.1 + Razor Pages + HttpClientFactory + Refit + Consumo de API REST + Application Insights
    ASP.NET Core 3.1 + Razor Pages + HttpClientFactory + Refit + Consumo de API REST + Application Insights

---
+++ SiteConsumoAPIContagem/Pages/Index.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using SiteConsumoAPIContagem.Interfaces;
+
+namespace SiteConsumoAPIContagem.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet(
+            [FromServices]IConfiguration configuration,
+            [FromServices]ILogger<IndexModel> logger,
+            [FromServices]IContagemClient contagemClient)
+        {
+            var resultado = contagemClient.GetResultado().Result;
+            logger.LogInformation($"APIContagem: {resultado}");
+
+            TempData["ValorContador"] = resultado.ValorAtual;
+            TempData["ResultadoAPIContagem"] =
+                JsonSerializer.Serialize(resultado);
+            TempData["Saudacao"] = configuration["Mensagens:Saudacao"];
+            TempData["Local"] = configuration["Mensagens:Local"];
+        }
+    }
+}
